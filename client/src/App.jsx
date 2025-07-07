69f20fdc8bc280c3e506c9c721f69f74ac70a842
mahir <mahir21263@iiitd.ac.in>
2025-07-07 12:58:24 +0530

    vercel analytics

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,15 @@ import './styles.css';
 import Problems from './pages/problems';
 import { VisitCounter } from './pages/problems';
 import { useState } from 'react';
-
+import { Analytics } from '@vercel/analytics/react';
 function App() {
   const [showCounter, setShowCounter] = useState(false);
 
   return (
     <div className="container">
       <Problems onLoaded={() => setShowCounter(true)} />
-      {showCounter && <VisitCounter />}
+      {/* {showCounter && <VisitCounter />} */}
+      <Analytics />
     </div>
   );
 }
