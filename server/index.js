b1c0f73435b6a3ef9af9d4e2da29bd356baa89e9
mahir <mahir21263@iiitd.ac.in>
2025-07-07 15:27:27 +0530

    render

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import cors from 'cors'
+import connectDB from './db.js'
+import dotenv from 'dotenv'
+const app = express();
+const PORT = process.env.PORT || 10000;
+const HOST = '0.0.0.0';
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Backend is running!');
+});
+
+app.listen(PORT, HOST, () => {
+    console.log(`Server running on http://${HOST}:${PORT}`);
+});
+import problemRoutes from './routes/problemRoute.js';
+import feedbackRoutes from './routes/feedbackroute.js';
+
+app.use('/api/problems', problemRoutes);
+app.use('/api/feedback', feedbackRoutes);
+import visitRoutes from './routes/visitroute.js';
+
+app.use('/api/visits', visitRoutes);
